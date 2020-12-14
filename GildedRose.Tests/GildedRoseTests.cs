731ad5c23f93c78e66db011098032f3883218f00
Javier <jmartin@grupoloyal.com>
2020-12-14 14:37:13 +0100

    GildedRose

---
+++ GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GildedRose;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Tests {
+    [TestClass()]
+    public class GildedRoseTests {
+        [TestMethod()]
+        public void GildedRoseTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateQualityTest() {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
