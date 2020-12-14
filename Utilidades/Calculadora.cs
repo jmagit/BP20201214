0287d07f1b00405162afbbe726ed973dc3be747e
Javier <jmartin@grupoloyal.com>
2020-12-14 12:41:30 +0100

    Calculadora

---
+++ Utilidades/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades {
+    public class Calculadora {
+
+        public double Suma(double a, double b) {
+            return a + b;
+        }
+        public double Resta(double a, double b) {
+            return a - b;
+        }
+        public double Multiplica(double a, double b) {
+            return a * b;
+        }
+        public double Divide(double a, double b) {
+            return a / b;
+        }
+    }
+}
