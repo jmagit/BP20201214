73c33de96059799bf27a09f98b28c3b2edb6fb52
Javier <jmartin@grupoloyal.com>
2021-04-16 11:37:22 +0200

    Compresor

---
+++ Ocultacion/Compresor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace Ocultacion {
+    public class Compresor {
+        public static void Comprimir(String origen, String destino) {
+            using (var salida = new FileStream(destino, FileMode.Create)) {
+                using (var compressionStream = new GZipStream(salida, CompressionMode.Compress)) {
+                    using (var lector = new StreamReader(new FileStream(origen, FileMode.Open))) {
+                        using (StreamWriter escritor = new StreamWriter(compressionStream)) {
+                            escritor.Write(lector.ReadToEnd());
+                        }
+                    }
+                }
+            }
+        }
+        public static string Descomprimir(String resource) {
+            System.Reflection.Assembly _assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            //Stream _fileStream;
+            //_fileStream = _assembly.GetManifestResourceStream(resource);
+
+//            TextReader tr = new StreamReader(_fileStream, System.Text.Encoding.GetEncoding(1252));
+            using (var entrada = _assembly.GetManifestResourceStream(_assembly.GetName().Name + "." + resource)) {
+                using (GZipStream compressionStream = new GZipStream(entrada, CompressionMode.Decompress)) {
+                    using (StreamReader lector = new StreamReader(compressionStream)) {
+                        return lector.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
