45d4626a54d7a9d6a8e9c8422eeb9a1191b2bb95
Javier <jmartin@grupoloyal.com>
2021-04-16 10:30:57 +0200

    Compresor

---
+++ Ocultacion/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+
+namespace Ocultacion {
+    class Program {
+        static void Main(string[] args) {
+            string key = "2ujsLq7QV+l9/oNrE70xhSJ2kgv+v9ek7EbLwiCAoZw=";
+            string IV = "wpKnuXS2mkOhuKGknRmZSQ==";
+            Aes aes = Aes.Create();
+
+            //byte[] keyAes = Convert.FromBase64String(key);
+            //byte[] IVAes = Convert.FromBase64String(IV);
+            //using (MemoryStream msEncrypt = new MemoryStream()) {
+            //    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, 
+            //            aes.CreateEncryptor(Convert.FromBase64String(key), 
+            //            Convert.FromBase64String(IV)), CryptoStreamMode.Write)) {
+            //        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt)) {
+            //            swEncrypt.Write("SELECT ... FROM");
+            //        }
+            //        Debug.WriteLine(Convert.ToBase64String(msEncrypt.ToArray()));
+            //    }
+            //}
+            // Program kk;
+            // kk.GetType().Assembly.
+            //using (MemoryStream msEncrypt = new MemoryStream(Convert.FromBase64String("oUuKgGzzAXVtb4ET0AGiYg=="))) {
+            //    using (CryptoStream csDecrypt = new CryptoStream(msEncrypt, aes.CreateDecryptor(Convert.FromBase64String(key),
+            //            Convert.FromBase64String(IV)), CryptoStreamMode.Read)) {
+            //        using (StreamReader srDecrypt = new StreamReader(csDecrypt)) {
+            //            Debug.WriteLine(srDecrypt.ReadToEnd());
+            //        }
+            //    }
+            //}
+
+            //using (MemoryStream msEncrypt = new MemoryStream()) {
+            //    using (GZipStream compressionStream = new GZipStream(msEncrypt, CompressionMode.Compress)) {
+            //        using (StreamWriter swEncrypt = new StreamWriter(msEncrypt)) {
+            //            swEncrypt.Write("SELECT ... FROM");
+            //        }
+            //        Debug.WriteLine(Convert.ToBase64String(msEncrypt.ToArray()));
+            //    }
+            //}
+            using (MemoryStream msEncrypt = new MemoryStream(Convert.FromBase64String("U0VMRUNUIC4uLiBGUk9N"))) {
+                using (GZipStream compressionStream = new GZipStream(msEncrypt, CompressionMode.Decompress)) {
+                    using (StreamReader srDecrypt = new StreamReader(msEncrypt)) {
+                        Debug.WriteLine(srDecrypt.ReadToEnd());
+                    }
+                }
+            }
+        }
+    }
+}
